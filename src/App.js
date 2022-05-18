66503c1859074a37becc62b5e7f020b4b9861fa2
Darek Radke <dradmclad@gmail.com>
2022-05-17 19:30:41 -0500

    basic canvas implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    const c = document.getElementById("myCanvas");
+    const ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+  }, []);
+
+  const addPoint = () => {
+    const ctx = document.getElementById("myCanvas").getContext("2d");
+    ctx.beginPath();
+    ctx.arc(100, 50, 10, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#003300';
+    ctx.stroke();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h1>HTML5 Canvas + React.js</h1>
+      <canvas
+        id="myCanvas"
+        width="200"
+        height="100"
+        style={{ border: "1px solid #d3d3d3" }}
+      >
+        Your browser does not support the HTML canvas tag.
+      </canvas>
+      <button onClick={() => addPoint()}>Add Point</button>
     </div>
   );
 }
